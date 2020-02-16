6b4b6f3a823072145a6a9efadc0ce6b04fc98c9f
Stino - Linksonder - Smulders <linksonder@gmail.com>
2020-02-16 22:42:45 +0100

    basics working

--- js/app.js
+++ js/app.js
@@ -1,29 +1,25 @@
-navigator.mediaDevices
-  .getUserMedia({
-    audio: true
-  })
-  .then(stream => {
-    rec = new MediaRecorder(stream);
-    rec.ondataavailable = e => {
-      audio.push(e.data);
-      if (rec.state == "inactive") {
-        let blob = new Blob(audio, {
-          type: "audio/x-mpeg-3"
-        });
-        recordedAudio.src = URL.createObjectURL(blob);
-        recordedAudio.controls = true;
-        audioDownload.href = recordedAudio.src;
-        audioDownload.download = "audio.mp3";
-        audioDownload.innerHTML = "Download";
-        submit(blob);
-      }
-    };
-  })
-  .catch(e => console.log(e));
+function showAudio(sound){
 
-// TODO: This needs work. Submit button currently does not do anything.
-// Also, page does not get reloaded and therefore the results are not shown.
-// The POST request has to be done without AJAX.
+  console.log(sound);
+  let audio = document.createElement('audio');
+  audio.src = URL.createObjectURL(sound.blob);
+  audio.controls = true;
+  // audioDownload.href = recordedAudio.src;
+  // audioDownload.download = "audio.mp3";
+  // audioDownload.innerHTML = "Download";
+
+  document.getElementById('sounds').appendChild(audio);
+
+}
+
+function storeAudio(blob){
+  
+  let id = document.querySelector("#id").value;
+  soundService.addSound(id, blob).then((sound) => {
+    //showSound(sound);
+  })
+}
+
 
 function startRecording() {
   startRecord.disabled = true;
@@ -39,24 +35,34 @@ function stopRecording() {
   rec.stop();
 }
 
-function submit(blob) {
-  var reader = new window.FileReader();
-  reader.readAsDataURL(blob);
-  reader.onloadend = function() {
-    var fd = new FormData();
-    base64data = reader.result;
-    console.log(base64data);
-    fd.append("file", base64data, "audio.mp3");
-    $.ajax({
-      type: "POST",
-      url: "/",
-      data: fd,
-      cache: false,
-      processData: false,
-      contentType: false,
-      enctype: "multipart/form-data"
-    }).done(function(data) {
-      console.log(data);
-    });
-  };
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true
+  })
+  .then(stream => {
+    rec = new MediaRecorder(stream);
+    rec.ondataavailable = e => {
+      audio.push(e.data);
+      if (rec.state == "inactive") {
+        let blob = new Blob(audio, {
+          type: "audio/x-mpeg-3"
+        });
+
+        storeAudio(blob);
+       
+        //submit(blob);
+      }
+    };
+  })
+  .catch(e => console.log(e));
+
+function init() {
+  soundService.open().then(() => {
+    soundService.getAllSounds().then(sounds => {
+      console.log(sounds);
+      sounds.forEach(s => showAudio(s));
+    })
+  })
 }
+
+window.addEventListener("DOMContentLoaded", init, false);
---
+++ js/database.js
@@ -0,0 +1,192 @@
+var soundService = {};
+initIndexDB();
+
+/** 
+ * File layout:
+ * - Interface functions
+ * - Scoped functions
+ */
+
+/**
+ * Opens a connection to IndexDB and the 'sound' objec store.
+ * Returns a promise with the sound store.
+ */
+soundService.open = function() {
+  return new Promise((resolve, reject) => {
+    var version = 1;
+    var request = indexedDB.open("voorlezen", version);
+  
+    // We can only create Object stores in a versionchange transaction.
+    request.onupgradeneeded = function(e) {
+      var db = e.target.result;
+  
+      // A versionchange transaction is started automatically.
+      e.target.transaction.onerror = onerror;
+  
+      if(db.objectStoreNames.contains("sound")) {
+        db.deleteObjectStore("sound");
+      }
+  
+      var store = db.createObjectStore("sound",
+        {keyPath: "id"});
+    };
+  
+    //
+    request.onsuccess = function(e) {
+      soundService.db = e.target.result; //add DB to sound service
+      resolve(soundService);
+    };
+  
+    //request.onerror = soundService.indexedDB.onerror;
+    request.onerror = (e) => reject(e);
+  })
+  
+};
+
+soundService.addSound = function(id, blob) {
+  return new Promise((resolve, reject) => {
+    
+    let arraybuffer = blobToArrayBuffer(blob);
+
+    //read array buffer
+    arraybuffer.then(sound => {
+  
+      var trans = soundService.db.transaction(["sound"], "readwrite");
+      var store = trans.objectStore("sound");
+  
+      var data = {
+        "blob": sound,
+        "id": id
+      };
+  
+      var request = store.put(data);
+  
+      request.onsuccess = function(e) {
+        resolve(data);
+      };
+  
+      request.onerror = function(e) {
+        onError(e); //internal
+        reject(e);
+      };
+    })
+  })
+};
+
+soundService.deleteSound = function(id) {
+  return new promise((resolve, reject) => {
+    var trans = soundService.db.transaction(["todo"], "readwrite");
+    var store = trans.objectStore("todo");
+  
+    var request = store.delete(id);
+  
+    request.onsuccess = function(e) {
+      resolve();
+    };
+  
+    request.onerror = function(e) {
+      onError(e);
+      reject(e);
+    };
+  })
+  
+};
+
+
+soundService.getAllSounds = function() {
+
+
+  var trans = soundService.db.transaction(["sound"], "readwrite");
+  var store = trans.objectStore("sound");
+  var allSounds = [];
+
+  return new Promise((resolve, reject) => {
+
+    //count how many successes will trigger to get all the items
+    store.count().onsuccess = (e) => {
+
+      var counter = 0;
+      var count = e.target.result;
+
+       // Get everything in the store;
+      var keyRange = IDBKeyRange.lowerBound(0);
+      var cursorRequest = store.openCursor(keyRange);
+
+      console.log(`Getting ${count} sounds`)
+    
+      cursorRequest.onsuccess = function(e) {
+
+        var result = e.target.result;
+        if(!!result == false)
+          return;
+
+        let sound = result.value;
+        sound.blob = arrayBufferToBlob(sound.blob);
+        allSounds.push(sound);
+
+        counter++;
+
+        if(counter < count){  
+          result.continue();
+        }
+        else{
+          resolve(allSounds); //emit response
+        }
+      };
+    
+      cursorRequest.onerror = (error) => {
+        onError(e);
+        reject(e);
+      }
+    }
+  }); 
+};
+
+/**
+ * initIndexDB()
+ * This scoped function initlializes IndexDB for different browsers
+ */
+function initIndexDB(){
+  window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
+
+  if ('webkitIndexedDB' in window) {
+   window.IDBTransaction = window.webkitIDBTransaction;
+    window.IDBKeyRange = window.webkitIDBKeyRange;
+  }
+}
+
+/**
+ * OnError
+ * This scoped function is used internally
+ * @param {*} error 
+ */
+function onError(error){
+  console.log("Sound service error:")
+  console.log(error);
+}
+
+/**
+ * arrayBufferToBlob
+ * This scoped function transforms an array buffer into a blob.
+ * @param {*The buffer to transform } buffer 
+ * @param {* The type, default mp4} type 
+ */
+function arrayBufferToBlob(buffer, type) {
+  return new Blob([buffer], {type: type});
+}
+
+/**
+ * blobToArrayBuffer
+ * This scoped function transforms a blob into an array buffer.
+ * @param {*The sound blob} blob 
+ */
+function blobToArrayBuffer(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.addEventListener('loadend', (e) => {
+      resolve(reader.result);
+    });
+    reader.addEventListener('error', reject);
+    reader.readAsArrayBuffer(blob);
+  });
+}
